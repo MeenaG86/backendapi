da6e788c5030512d133ab0cdc436bddcc87181a2
MeenaG86 <meeash2021@gmail.com>
2026-05-05 13:14:23 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [status, setStatus] = useState("");
   const [message, setMessage] = useState("");
 
-  const API_URL = "https://backendapi-cyan.vercel.app/students";
+  const API_URL = "https://backendapi-bice.vercel.app/students";
 
   function getStudents() {
     axios
